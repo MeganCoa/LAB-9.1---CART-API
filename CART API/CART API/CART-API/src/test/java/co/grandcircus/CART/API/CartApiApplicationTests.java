af61a5880ccd85e7bb8092cf01a4037a3c2e6a48
Megan Coats <ctsmgn@yahoo.com>
2022-08-20 18:53:56 -0400

    submission

---
+++ CART API/CART API/CART-API/src/test/java/co/grandcircus/CART/API/CartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.CART.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
